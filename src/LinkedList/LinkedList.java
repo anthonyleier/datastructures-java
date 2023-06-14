a9a029d4b52335c20c6622e28fbca45cc9e1a2ef
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-06-14 15:57:47 -0300

    Melhorias no LinkedList

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,46 +1,69 @@
 package LinkedList;
 
 public class LinkedList {
-    private final Node head;
+    private Node head;
     private Node last;
     private int size;
 
-    LinkedList(int value) {
-        this.head = new Node(value);
-        this.last = this.head;
+    LinkedList() {
+        this.head = null;
+        this.last = null;
         this.size = 0;
     }
 
     public void add(int value) {
         Node node = new Node(value);
-        this.last.setNext(node);
-        this.last = node;
+        if (this.head == null) {
+            this.head = node;
+            this.last = this.head;
+        } else {
+            this.last.next = node;
+            this.last = node;
+        }
         this.size++;
     }
 
-    public int getIndex(int index) {
-        if (index < 0 || index > this.size) throw new IndexOutOfBoundsException("Index fora de alcance");
+    public int get(int index) {
+        if (index < 0 || index > this.size - 1) throw new IndexOutOfBoundsException("Index fora de alcance");
 
         Node currentNode = this.head;
         int counter = 0;
 
-        while (counter != index) {
-            currentNode = currentNode.getNext();
+        while (counter != index && currentNode != null) {
+            currentNode = currentNode.next;
             counter++;
         }
 
-        return currentNode.getValue();
+        assert currentNode != null;
+        return currentNode.value;
     }
 
     @Override
     public String toString() {
         StringBuilder message = new StringBuilder();
         Node currentNode = this.head;
+
         while (currentNode != null) {
             message.append(currentNode);
             message.append(", ");
-            currentNode = currentNode.getNext();
+            currentNode = currentNode.next;
         }
+
         return message.toString();
+    }
+
+    public static class Node {
+        private final int value;
+        private Node next;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(this.value);
+        }
     }
 }
